95b4b1a09ec5019b7bcbe04a31fbfc3e92b7566f
Jason <jason.rgd@gmail.com>
2024-06-09 01:26:11 +1000

    initial commit

---
+++ configuration/mongo/mongo-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sleep 10
+
+mongosh --host mongo1:27017 <<EOF
+rs.initiate({
+  _id : "rs0",
+  members: [
+    { _id: 0, host: "mongo1:27017" },
+    { _id: 1, host: "mongo2:27017" },
+    { _id: 2, host: "mongo3:27017" }
+  ]
+});
+EOF
